1f9c2be7233cee32faed142eda13164e223ca7a9
Royc4515 <Roy.y.carmelli@gmail.com>
2026-01-13 00:54:48 +0200

    Implement maxProfit function for stock prices

--- interview_prep/buy_and_sell.c
+++ interview_prep/buy_and_sell.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int maxProfit(int* prices, int pricesSize) {
+    if (pricesSize < 2) return 0;
+    int max_pro = 0;
+    int min_buy = INFINITY;
+    for ( int i = 0; i< pricesSize; i++) {
+        if (prices[i]<min_buy){
+            min_buy= prices[i];
+        }
+        else if (prices[i]- min_buy > max_pro){
+            max_pro = prices[i]- min_buy;
+        }
+    }
+    return max_pro;
+}
 
